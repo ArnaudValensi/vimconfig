461feb03f5088f2f5db946875ac67a5bee248da4
Arnaud Valensi <arnaud.valensi@gmail.com>
2024-12-19 11:16:13 +0100

    Add a build command to build oob in a terminal

--- lua/user/terminal.lua
+++ lua/user/terminal.lua
@@ -6,13 +6,24 @@ vim.api.nvim_create_autocmd('TermOpen', {
     end,
 })
 
+local job_id = 0
+local terminal_buf = -1
+
 vim.keymap.set("n", "<space>vv", function()
   local build_cmd = "cd /mnt/d/dev/oob/repos/oob-jai && jai.exe build.jai - build && ./build/game.exe --no-music\r\n"
-  local terminal_name = "BuildTerminal"
-  -- local terminal_buf = nil
 
-  vim.cmd.term()
+  if job_id == 0 or not vim.api.nvim_buf_is_valid(terminal_buf) then
+    vim.cmd("terminal")
+    terminal_buf = vim.api.nvim_get_current_buf()
+    job_id = vim.bo.channel
+  else
+    vim.api.nvim_set_current_buf(terminal_buf)
+  end
 
-  local job_id = vim.bo.channel
   vim.fn.chansend(job_id, { build_cmd })
+
+  -- Scroll to the bottom of the terminal
+  vim.api.nvim_buf_call(terminal_buf, function()
+    vim.cmd("normal! G")  -- Move to the end of the buffer
+  end)
 end)
